fdd1b6803ff5b6d3346941e96187b6c121f04a3a
kiddikn <kiddikn@gmail.com>
2020-10-25 20:41:59 +0900

    add pubsub test

--- go.mod
+++ go.mod
@@ -3,11 +3,13 @@ module shift-api
 go 1.14
 
 require (
-	github.com/Masterminds/semver v1.5.0 // indirect
+	cloud.google.com/go/pubsub v1.3.1
 	github.com/golang/protobuf v1.4.2
 	github.com/jinzhu/gorm v1.9.12
 	github.com/twitchtv/twirp v5.12.0+incompatible
+	go.opencensus.io v0.22.5 // indirect
 	go.uber.org/zap v1.15.0
-	google.golang.org/grpc v1.29.1
-	google.golang.org/protobuf v1.24.0
+	google.golang.org/api v0.33.0 // indirect
+	google.golang.org/grpc v1.31.1
+	google.golang.org/protobuf v1.25.0
 )
---
+++ main.go
@@ -0,0 +1,34 @@
+// Sample pubsub-quickstart creates a Google Cloud Pub/Sub topic.
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"cloud.google.com/go/pubsub"
+)
+
+func main() {
+	ctx := context.Background()
+
+	// Sets your Google Cloud Platform project ID.
+	projectID := "otakeslsc-215813"
+
+	// Creates a client.
+	client, err := pubsub.NewClient(ctx, projectID)
+	if err != nil {
+		log.Fatalf("Failed to create client: %v", err)
+	}
+
+	// Sets the id for the new topic.
+	topicID := "my-topic"
+
+	// Creates the new topic.
+	topic, err := client.CreateTopic(ctx, topicID)
+	if err != nil {
+		log.Fatalf("Failed to create topic: %v", err)
+	}
+
+	fmt.Printf("Topic %v created.\n", topic)
+}
